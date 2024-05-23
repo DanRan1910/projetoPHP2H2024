916078429508c0234a96a0be2b28a8c7154b38b5
Daniel <160975687+DanRan1910@users.noreply.github.com>
2024-05-22 22:10:48 -0300

    Add files via upload

---
+++ projetoPHP2H2024/exe03.php
@@ -0,0 +1,8 @@
+<?php 
+//Digite 10 valores numéricos e armazene em um vetor. 
+//Em seguida, solicite ao usuário um número para multiplicar todos os elementos do vetor. 
+//O programa deverá exibir o resultado da multiplicação do número dado pelo usuário em todos os elementos armazenados. 
+
+
+
+?>
